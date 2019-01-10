f736cd04c91aae79573de4ec159266fc06e7232f
Reddy Pothireddy <g.reddy.pothireddy@accenture.com>
2019-01-10 08:47:53 +0530

    changed the json structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,27 +9,75 @@ import * as d31 from 'd3'
 export class AppComponent {
   title = 'Code Hackathon';
 
-  x = {
-    locations: ["hyderabad", "bangalore"],
-    sites: {
-      "hyderabad": ["HDC1", "HDC2", "HDC3", "HDC4"],
-      "bangalore": ["BDC1", "BDC2", "BDC3", "BDC4"]
-    },
-    buildings: {
-      "hyderabadHDC1": ["towera", "towerb", "towerc"],
-      "hyderabadHDC2": ["towerd", "towere", "towerf"]
-    },
-    floors: {
-      "hyderabadHDC1towera": ['floor1', 'floor2', 'floor3'],
-      "hyderabadHDC1towerb": ['floor1', 'floor2', 'floor3']
-    },
-    bays: {
-      "hyderabadHDC1towerafloor1": ['bay1', 'bay2', 'bay3'],
-      "hyderabadHDC1towerafloor2": ['bay1', 'bay2', 'bay3'],
-    },
-    details: {
-      "hyderabadHDC1towerafloor1bay1": ["totalseats : 94", "serialnumber : 401 to 494"],
-      "hyderabadHDC1towerafloor1bay2": ["totalseats : 94", "serialnumber : 401 to 494"]
-    }
+
+  t = {
+    "Locations": [
+      {
+        "id": "1",
+        "name": "hyderabad"
+      },
+      {
+        "id": "2",
+        "name": "bangalore"
+      },
+      {
+        "id": "3",
+        "name": "chennai"
+      }
+    ],
+    "Sites": [
+      {
+        "id": "1",
+        "name": "HDC1",
+        "Locationsid": "1"
+      },
+      {
+        "id": "2",
+        "name": "HDC2",
+        "Locationsid": "1"
+      },
+      {
+        "id": "3",
+        "name": "HDC2",
+        "Locationsid": "1"
+      },
+      {
+        "id": "4",
+        "name": "BDC1",
+        "Locationsid": "2"
+      },
+      {
+        "id": "5",
+        "name": "BDC1",
+        "Locationsid": "2"
+      },
+      {
+        "id": "6",
+        "name": "BDC1",
+        "Locationsid": "2"
+      }
+    ],
+    "Buildings": [
+      {
+        "id": "1",
+        "name": "towerA",
+        "Sitesid": "1"
+      },
+      {
+        "id": "2",
+        "name": "towerB",
+        "Sitesid": "1"
+      },
+      {
+        "id": "3",
+        "name": "towerA",
+        "Sitesid": "2"
+      },
+      {
+        "id": "4",
+        "name": "towerB",
+        "Sitesid": "2"
+      }
+    ]
   }
 }
--- src/app/codeathonparent/codeathonparent.component.ts
+++ src/app/codeathonparent/codeathonparent.component.ts
@@ -14,6 +14,7 @@ export class CodeathonparentComponent implements OnInit {
   displayheaderonload
   constructor() { }
 
+  //extracts keynames(attributes) from object and pushes it to header variable.
   getkeys(x) {
     for (let i in x) {
       if (x.hasOwnProperty(i)) {
@@ -25,31 +26,38 @@ export class CodeathonparentComponent implements OnInit {
   ngOnInit() {
     this.getkeys(this.inputData)
     var firstColumn = this.header[0];
+    //display first column by default
     this.obj["display" + firstColumn] = true;
+    //data for first column
     this.obj[firstColumn] = this.inputData[firstColumn]
   }
 
   next(data, index) {
-    var currentColumnkey = this.header[index]
-    var nextcolumnkey = this.header[index + 1]
-    this.obj['temp'] = '';
+    this.hideOrDisplayColumns(index);
+    this.nextColumnData(data, index);
+  }
 
-    this.obj['temp' + currentColumnkey] = data;
-
-    for (var x = 0; x <= this.header.length; x++) {
+  hideOrDisplayColumns(index) {
+    for (let x = 0; x <= this.header.length; x++) {
       let next = this.header[x + 1];
-      let current = this.header[x];
       if (x <= (index)) {
         this.obj["display" + next] = true;
-        this.obj['temp'] = this.obj['temp'] + this.obj['temp' + current];
       }
       else {
         this.obj["display" + next] = false;
       }
     }
+  }
 
-    if (!this.inputData[nextcolumnkey][this.obj.temp])
-      this.obj["display" + nextcolumnkey] = false;
-    this.obj[nextcolumnkey] = this.inputData[nextcolumnkey][this.obj.temp]
+  nextColumnData(data, index) {
+    var currentColumnkey = this.header[index]
+    var nextcolumnkey = this.header[index + 1]
+    this.obj[nextcolumnkey] = [];
+    if (this.inputData[nextcolumnkey])
+      this.inputData[nextcolumnkey].forEach(element => {
+        if (element[currentColumnkey + "id"] === data.id) {
+          this.obj[nextcolumnkey].push(element)
+        }
+      })
   }
 }
